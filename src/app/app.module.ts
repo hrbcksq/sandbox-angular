1dc3e4a927d98aa97a037eba08d531e82dfb9015
Ruslan Kisly <rchisly@gmail.com>
2017-07-03 16:58:42 +0300

    First assigment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { AlertComponent } from './shared/alert/alert.component';
+import { WarningComponent } from './shared/warning/warning.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    AlertComponent,
+    WarningComponent
 ],
   imports: [
     BrowserModule,
