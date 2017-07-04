c743b10b2235d92646c22ce8b36ad06ef4639577
Ruslan Kisly <rchisly@gmail.com>
2017-07-04 11:19:45 +0300

    Applying CSS Classes Dynamically with ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+        .online {
+            background-color: green;
+        }
+    `]
 })
 export class ServerComponent implements OnInit {
     serverId = 10;
