0da2359a6f72074ea06d0ab469589fa3c8f16416
Ruslan Kisly <rchisly@gmail.com>
2017-07-20 16:34:29 +0300

    Getting Access to the Template & DOM with @ViewChild

--- src/app/view/dashboard/dashboard.component.ts
+++ src/app/view/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { ServerType } from '../../model/enum/server.type';
 import { Server } from '../../model/model.module';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,6 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
   current: Server;
   list: Server[];
+  @ViewChild('serverNameInput') nameInput: ElementRef;
 
   constructor() {
     this.current = new Server();
@@ -19,8 +20,8 @@ export class DashboardComponent implements OnInit {
   ngOnInit() {
   }
 
-  onAdd(nameInput?: HTMLInputElement) {
-    console.log(nameInput.value);
+  onAdd() {
+    console.log(this.nameInput.nativeElement.value);
     if (this.current.isValid()) {
       this.list.push(this.current);
     }
