f3ec0c11269d082d82b08e6513df2f7b4a39280a
Ruslan Kisly <rchisly@gmail.com>
2017-08-02 15:25:29 +0300

    ngSwitch ngSwitchCase ngSwitchDefault

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,15 @@ export class AppComponent {
   oddNumbers = [1, 3, 5, 7];
   evenNumbers = [2, 4, 6, 8];
   onlyOdd = false;
+  value = 1;
+
+  constructor() {
+    setInterval(() => {
+      if (this.value >= 10000) {
+        this.value = 1;
+      } else {
+        this.value *= 10;
+      }
+    }, 500)
+  }
 }
