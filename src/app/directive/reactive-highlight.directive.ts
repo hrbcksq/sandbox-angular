7ea392ecfde8ddb7a0fa1f6a2f968f4cd1226a0e
Ruslan Kisly <rchisly@gmail.com>
2017-08-02 14:04:14 +0300

    Binding to Directive Properties

--- src/app/directive/reactive-highlight.directive.ts
+++ src/app/directive/reactive-highlight.directive.ts
@@ -1,25 +1,31 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appReactiveHighlight]'
 })
 export class ReactiveHighlightDirective implements OnInit {
-  @HostBinding('style.background-color') backgroundColor = 'transparent';
+  @HostBinding('style.background-color') backgroundColor;
 
+  @Input() defaultColor = 'transparent';
+  @Input() highlightColor = 'lightgreen';
+  // tslint:disable-next-line:no-input-rename
+  @Input('appReactiveHighlight') alertColor = 'red';
   constructor(
     // private readonly ElementRef: ElementRef,
     // private readonly renderer: Renderer2
   ) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.backgroundColor = this.defaultColor;
+  }
 
   @HostListener('mouseenter') mouseEnter(event: Event) {
     // this.renderer.setStyle(this.ElementRef.nativeElement, 'background-color', 'pink');
-    this.backgroundColor = 'blue';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseLeave(event: Event) {
     // this.renderer.setStyle(this.ElementRef.nativeElement, 'background-color', 'transparent');
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 }
