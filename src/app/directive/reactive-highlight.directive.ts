ac7a8e87494bc78dacbbc9a8318a588ab32f3b7b
Ruslan Kisly <rchisly@gmail.com>
2017-08-02 12:16:50 +0300

    Using HostBinding to Bind to Host Properties

--- src/app/directive/reactive-highlight.directive.ts
+++ src/app/directive/reactive-highlight.directive.ts
@@ -1,22 +1,25 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appReactiveHighlight]'
 })
 export class ReactiveHighlightDirective implements OnInit {
+  @HostBinding('style.background-color') backgroundColor = 'transparent';
 
   constructor(
-    private readonly ElementRef: ElementRef,
-    private readonly renderer: Renderer2
+    // private readonly ElementRef: ElementRef,
+    // private readonly renderer: Renderer2
   ) { }
 
   ngOnInit(): void { }
 
   @HostListener('mouseenter') mouseEnter(event: Event) {
-    this.renderer.setStyle(this.ElementRef.nativeElement, 'background-color', 'pink');
+    // this.renderer.setStyle(this.ElementRef.nativeElement, 'background-color', 'pink');
+    this.backgroundColor = 'blue';
   }
 
   @HostListener('mouseleave') mouseLeave(event: Event) {
-    this.renderer.setStyle(this.ElementRef.nativeElement, 'background-color', 'transparent');
+    // this.renderer.setStyle(this.ElementRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
   }
 }
